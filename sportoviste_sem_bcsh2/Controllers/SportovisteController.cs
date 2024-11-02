fc6ff795d72e0f1f9fb87df8760f0255e6d23adc
apecin <120096461+apecin@users.noreply.github.com>
2024-11-02 16:24:52 +0100

    Sportoviste View a controller

---
+++ sportoviste_sem_bcsh2/Controllers/SportovisteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sportoviste_sem_bcsh2.Models;
+
+namespace sportoviste_sem_bcsh2.Controllers
+{
+    public class SportovisteController : Controller
+    {
+        public IActionResult Index()
+        {
+            var sportovisteList = new List<Sportoviste>
+        {
+            new Sportoviste { Id = 1, Nazev = "Sportoviště 1" },
+            new Sportoviste { Id = 2, Nazev = "Sportoviště 2" },
+            new Sportoviste { Id = 3, Nazev = "Sportoviště 3" }
+        };
+
+            return View(sportovisteList);
+        }
+    }
+
+}
--- sportoviste_sem_bcsh2/obj/Debug/net8.0/sportoviste_sem_bcsh2.AssemblyInfo.cs
+++ sportoviste_sem_bcsh2/obj/Debug/net8.0/sportoviste_sem_bcsh2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e319b583a53800ede60bd29fae27a521cf6b08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0845abca8f1aabcf7bfab2c34fb1a88ad744cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
