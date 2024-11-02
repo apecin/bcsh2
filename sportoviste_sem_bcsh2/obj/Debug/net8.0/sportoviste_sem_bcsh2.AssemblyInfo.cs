1d12697a8161d94ba11a4464a7c47b9d23a197cb
apecin <120096461+apecin@users.noreply.github.com>
2024-11-02 14:53:20 +0100

    hriste view

--- sportoviste_sem_bcsh2/obj/Debug/net8.0/sportoviste_sem_bcsh2.AssemblyInfo.cs
+++ sportoviste_sem_bcsh2/obj/Debug/net8.0/sportoviste_sem_bcsh2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed78f9f9b1599c34f68e05d67248bf46c6b60e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2131a1db7bd1c6331f7203b888d54a237584bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sportoviste_sem_bcsh2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
